2a7053bd8e1382a7534cfe58e8b524fe5080bb52
Mathieu Hubbard <mathieuhubbard@gmail.com>
2021-05-16 13:50:00 -0400

    add border to image

--- js/all.js
+++ js/all.js
@@ -189,11 +189,11 @@
             
             if ($(window).scrollTop() > 10) {
                 $(".js-transparent").removeClass("transparent");
-                $(".main-nav, .nav-logo-wrap .logo, .mobile-nav").addClass("small-height");
+                $(".main-nav, .nav-logo-wrap .logo, .mobile-nav").addClass("small-height").addClass("dark");
             }
             else {
                 $(".js-transparent").addClass("transparent");
-                $(".main-nav, .nav-logo-wrap .logo, .mobile-nav").removeClass("small-height");
+                $(".main-nav, .nav-logo-wrap .logo, .mobile-nav").removeClass("small-height").removeClass("dark");
             }
             
             
